b2ba6272cafbd1fa14cb37fcc9e31e13855c1d9c
rmtechup1 <rmtechup1@gmail.com>
2022-12-28 03:20:34 +0530

    xmlhttpreq - display flags, official name, region, subregion, population in console

---
+++ asmt/d4/xmlhttpreq_displayCountryDetailsInConsole.js
@@ -0,0 +1,20 @@
+
+let req = new XMLHttpRequest();
+
+req.open('GET', 'https://restcountries.com/v3.1/all');
+
+req.send();
+
+req.onload = function(){
+    parsedResp = JSON.parse(this.response);
+    for(i=0; i<parsedResp.length; ++i){
+    console.log(parsedResp[i].flag);
+    console.log('official name: [', parsedResp[i].name.official, 
+                '], flag: [', parsedResp[i].flag, 
+                '], region: [', parsedResp[i].region, 
+                '], subregion: [', parsedResp[i].subregion, 
+                '], population: [', parsedResp[i].population,
+                ']');
+    }  
+}
+//why cant we use ()=>{} syntax here for the callback funtion???
